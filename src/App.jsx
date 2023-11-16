dd63dd93b3c28e7048246d64b2292bae2f81589c
rootel <ckcksdl135@naver.com>
2023-11-16 16:34:51 +0900

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Topbanner from "./components/Topbanner";
+
 const App = () => {
   return (
     <>
-      <div className="h-[42px] bg-purple-200">Top banner</div>
+      <Topbanner />
       <header className="bg-red-100 h-16">Header</header>
       <div className="bg-blue-100 h-[400px]">Banner</div>
       <section className="mt-20 bg-green-100 max-w-screen-xl mx-auto px-6">
